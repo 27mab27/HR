2ecce1df11f191a840e2269aa1d51f607df68a0e
Mohammed Alburayk <m.a.b.201120@gmail.com>
2024-07-17 09:39:26 +0300

    ADD requirement

--- HR/base/views.py
+++ HR/base/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+import joblib
 from .forms import RecruitmentData
 from joblib import load
 import pickle
 import os
 
-model_path = os.path.join('savedModels', 'LR_model_imb.pkl')
+model_path = os.path.join('savedModels', 'best_stacking_model.pkl')
 try:
     with open(model_path, 'rb') as file:
-        model = pickle.load(file)
-    print("Model loaded successfully.")
+        model = joblib.load(model_path)    
+        print("Model loaded successfully.")
 except FileNotFoundError:
     print(f"Error: The file {model_path} was not found.")
 except pickle.UnpicklingError:
@@ -20,16 +21,41 @@ except Exception as e:
 
 
 def Home (request):
-    form = RecruitmentData()
+    y_pred=None
+    form = RecruitmentData(request.POST)
     if request.method == 'POST':
         if form.is_valid():
-            object1 = form.save(commit=False)
-            return redirect('home')
+            object1= form.save(commit=False)
+            prediction_input = [
+                        [
+                            object1.age,
+                            object1.gender,
+                            object1.education_level,
+                            object1.experience_years,
+                            object1.previous_companies,
+                            object1.distance_from_company,
+                            object1.interview_score,
+                            object1.skill_score,
+                            object1.personality_score,
+                            object1.recruitment_strategy
+                        ]
+                    ]        
+            y_pred = model.predict([[object1.age, object1.gender, object1.education_level, object1.experience_years,
+                                         object1.previous_companies, object1.distance_from_company, object1.interview_score,
+                                         object1.skill_score, object1.personality_score, object1.recruitment_strategy]])
+            y_pred = y_pred[0]
         else:
             messages.error(request, 'An error occurred during registration')
+    if y_pred == 0:
+        y_pred="NOT HIRED"
+    if y_pred == 1:
+        y_pred="HIRED"
+        
+    
+        
+    
 
 
-
-    comtex={"form":form}
+    comtex={"form":form,"y_pred":y_pred}
 
     return render(request,'home.html',comtex)
