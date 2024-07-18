b95758d9ae589cd6620f3c77a153443ccecc30a5
Mohanad Alzeyadi <mohanad.alzyadii@gmail.com>
2024-07-18 00:54:31 -0700

    Adding uesr Login

--- HR/base/urls.py
+++ HR/base/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('',views.Home,name="Home")
+   path('',views.Home,name="home"),
+   path('login/', views.LoginPage, name="login"),
+   path('logout/', views.logoutUser, name="logout"),
+   path('register/', views.registerPage, name="register"),
+
+
+
 ]
--- HR/base/views.py
+++ HR/base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth.forms import UserCreationForm 
 import joblib
 from .forms import RecruitmentData
 from joblib import load
@@ -18,8 +22,53 @@ except pickle.UnpicklingError:
 except Exception as e:
     print(f"An error occurred while loading the model: {e}")
 
+def LoginPage(request):
+    page = 'Login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+    context = {'page':page}
+    return render(request, 'login_register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'login_register.html', {'form': form})
+
+
+@login_required(login_url='login')
 def Home (request):
     y_pred=None
     form = RecruitmentData(request.POST)
